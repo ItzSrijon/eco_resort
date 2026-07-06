2b0c96decb8d27baa2a064305ce87a5319882da7
Piya <shabnampiya07@gmail.com>
2026-07-06 21:23:52 +0600

    new pakages added

---
+++ src/main/java/Piya/helloViewController.java
@@ -0,0 +1,4 @@
+package Piya;
+
+public class helloViewController {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.summer.sectiton1.group7.eco_resort {
 
     opens com.summer.sectiton1.group7.eco_resort to javafx.fxml;
     exports com.summer.sectiton1.group7.eco_resort;
+    exports Piya;
+    opens Piya to javafx.fxml;
 }
