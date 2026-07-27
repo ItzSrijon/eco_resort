edf662103cd8c3d7249026de5687ce7c1a899390
Siam <2521090@iub.edu.bd>
2026-07-28 01:44:39 +0600

    Gym manager goal 2

---
+++ src/main/java/com/summer/section1/group7/eco_resort/Siam/UpdateMembershipStatusController.java
@@ -0,0 +1,45 @@
+package com.summer.section1.group7.eco_resort.Siam;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class UpdateMembershipStatusController
+{
+    @javafx.fxml.FXML
+    private TextField phoneNumberTF;
+    @javafx.fxml.FXML
+    private TextField guestNameTF;
+    @javafx.fxml.FXML
+    private TextField guestIDTF;
+    @javafx.fxml.FXML
+    private TextField emailAddressTF;
+    @javafx.fxml.FXML
+    private ComboBox newStatusCB;
+    @javafx.fxml.FXML
+    private TextField currentStatusTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboardOA(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void loadGuestOA(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void registerMemberOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchmembershipOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateStatusOA(ActionEvent actionEvent) {
+    }
+}
