25e7f1e79206ad469fa177b65982323ec7d60d68
Siam <2521090@iub.edu.bd>
2026-07-27 23:01:07 +0600

    Gym manager goal 1 fxml

---
+++ src/main/java/com/summer/section1/group7/eco_resort/Siam/RegisterGymManagerController.java
@@ -0,0 +1,37 @@
+package com.summer.section1.group7.eco_resort.Siam;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class RegisterGymManagerController
+{
+    @javafx.fxml.FXML
+    private ComboBox<String> membershipPackageCB;
+    @javafx.fxml.FXML
+    private TextField phoneNumberTF;
+    @javafx.fxml.FXML
+    private TextField guestNameTF;
+    @javafx.fxml.FXML
+    private TextField guestIDTF;
+    @javafx.fxml.FXML
+    private ComboBox<Integer> membershipDurationCB;
+    @javafx.fxml.FXML
+    private TextField emailAddressTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadGuestOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registerMemberOA(ActionEvent actionEvent) {
+    }
+}
