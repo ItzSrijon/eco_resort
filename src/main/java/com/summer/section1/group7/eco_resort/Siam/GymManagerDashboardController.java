1e08906b84493ceb2c0b92c3f4ab2abf47519c86
Siam <2521090@iub.edu.bd>
2026-07-27 22:26:47 +0600

    Gym manager dashboard

---
+++ src/main/java/com/summer/section1/group7/eco_resort/Siam/GymManagerDashboardController.java
@@ -0,0 +1,18 @@
+package com.summer.section1.group7.eco_resort.Siam;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class GymManagerDashboardController
+{
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+
+
+    @javafx.fxml.FXML
+    public void registerNewGymMemberOA(ActionEvent actionEvent) {
+    }
+}
