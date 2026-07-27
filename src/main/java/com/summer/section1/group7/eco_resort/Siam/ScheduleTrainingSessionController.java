06107229c5ed1f86704712ccddd5865b38838297
Siam <2521090@iub.edu.bd>
2026-07-28 02:48:16 +0600

    renamed fxml file and another scene designed

---
+++ src/main/java/com/summer/section1/group7/eco_resort/Siam/ScheduleTrainingSessionController.java
@@ -0,0 +1,41 @@
+package com.summer.section1.group7.eco_resort.Siam;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class ScheduleTrainingSessionController
+{
+    @javafx.fxml.FXML
+    private DatePicker sessionDateDP;
+    @javafx.fxml.FXML
+    private TextField phoneNumberTF;
+    @javafx.fxml.FXML
+    private TextField guestNameTF;
+    @javafx.fxml.FXML
+    private Button loadGuestOA11;
+    @javafx.fxml.FXML
+    private ComboBox<String> trainerCB;
+    @javafx.fxml.FXML
+    private ComboBox<String> sessionTimeCB;
+    @javafx.fxml.FXML
+    private TextField guestIdTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadGuestOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleSessionOA(ActionEvent actionEvent) {
+    }
+}
